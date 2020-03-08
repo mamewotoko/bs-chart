473ba262d3263b51a0576593202cb2f430187636
Takashi Masuyama <mamewotoko@gmail.com>
2020-03-08 15:31:21 +0900

    add line sample

--- src/chartjs.ml
+++ src/chartjs.ml
@@ -1,24 +1,66 @@
 open Webapi.Canvas
 type t
 
-(* bar | line | bubble *)
-type chart_type = string
+(* line | bar | radar | pie | doughnut | polarArea | bubble | scatter *)
+type chart_type = [
+  | `Line [@bs.as "line"]
+  | `Bar [@bs.as "bar"]
+  | `Radar [@bs.as "radar"]
+  | `Pie [@bs.as "pie"]
+  | `Doughnut [@bs.as "doughnut"]
+  | `PolarArea [@bs.as "plarArea"]
+  | `Bubble [@bs.as "bubble"]
+  | `Scatter [@bs.as "scatter"]
+  ] [@@bs.deriving jsConverter]
 type color_t = string
 
 type ticks_opt_t = {
     beginAtZero: bool
   } [@@bs.deriving {abstract = light}]
 
+type scale_label_t = {
+    display: bool [@bs.optional];
+    labelString: string [@bs.optional];
+  } [@@bs.deriving {abstract = light}]
+                 
 type axis_opt_t = {
-    ticks: ticks_opt_t
+    (* category, linear *)
+    type_: string  [@bs.as "type"] [@bs.optional];
+    (* bottom *)
+    position: string [@bs.optional];
+    ticks: ticks_opt_t  [@bs.optional];
+    display: bool  [@bs.optional];
   } [@@bs.deriving {abstract = light}]
 
+(* TODO: axisID -> config *)
 type scales_opt_t = {
-    yAxes: axis_opt_t array
+    xAxes: axis_opt_t array [@bs.optional];
+    yAxes: axis_opt_t array [@bs.optional];
   } [@@bs.deriving {abstract = light}]
 
+type hover_t = {
+    mode: string [@bs.optional]
+  } [@@bs.deriving {abstract = light}]
+
+type title_t = {
+    display: bool [@bs.optional];
+    text: string [@bs.optional];
+  } [@@bs.deriving {abstract = light}]
+
+type tool_tips_t = {
+    (* index *)
+    mode: string [@bs.optional];
+    intersect: bool [@bs.optional];
+  } [@@bs.deriving {abstract = light}]
+             
 type opt_t = {
-    scales: scales_opt_t
+    responsive: bool [@bs.optional];
+    title: title_t [@bs.optional];
+    tool_tips: tool_tips_t [@bs.optional];
+    showLines: bool [@bs.optional];
+    spanGaps: bool [@bs.optional];
+    hover: hover_t [@bs.optional];
+    scales: scales_opt_t [@bs.optional];
   } [@@bs.deriving {abstract = light}]
 
 type dataset_t = {
@@ -27,6 +69,7 @@ type dataset_t = {
   backgroundColor: color_t array [@bs.optional];
   borderColor: color_t array [@bs.optional];
   borderWidth: float [@bs.optional];
+  fill: bool [@bs.optional];
   } [@@bs.deriving {abstract = light}]
 
 type data_t = {
@@ -35,9 +78,9 @@ type data_t = {
   } [@@bs.deriving {abstract = light}]
 
 type param_t = {
-    type_: chart_type [@bs.as "type"] [@bs.optional];
+    type_: string [@bs.as "type"] [@bs.optional];
     data: data_t;
-    options: opt_t;
+    options: opt_t [@bs.optional];
   } [@@bs.deriving {abstract = light}]
    
 external make: Canvas2d.t -> param_t -> t = "Chart" [@@bs.new]
--- src/main.ml
+++ src/main.ml
@@ -4,49 +4,84 @@ open Canvas
    
 exception Not_found_element of string
 
+let param1 = 
+  param_t
+    ~type_:(chart_typeToJs `Bar)
+    ~data:(data_t
+             ~labels:[|"Red"; "Blue"; "Yellow"; "Green"; "Purple"; "Orange"|]
+             ~datasets:([|dataset_t
+                            ~label:"# of Votes"
+                            ~data:[|12.; 19.; 3.; 5.; 2.; 3.|]
+                            ~backgroundColor:[|
+                              "rgba(255, 99, 132, 0.2)";
+                              "rgba(54, 162, 235, 0.2)";
+                              "rgba(255, 206, 86, 0.2)";
+                              "rgba(75, 192, 192, 0.2)";
+                              "rgba(153, 102, 255, 0.2)";
+                              "rgba(255, 159, 64, 0.2)"
+                            |]
+                            ~borderColor:[|
+                              "rgba(255, 99, 132, 1)";
+                              "rgba(54, 162, 235, 1)";
+                              "rgba(255, 206, 86, 1)";
+                              "rgba(75, 192, 192, 1)";
+                              "rgba(153, 102, 255, 1)";
+                              "rgba(255, 159, 64, 1)"
+                            |]
+                            ~borderWidth:1.
+                            ();
+                        |])
+    )
+    ~options:(opt_t
+                ~scales:(scales_opt_t
+                           ~yAxes: [|
+                             axis_opt_t
+                               ~ticks:(ticks_opt_t ~beginAtZero:true)
+                               ()
+                           |]
+                           ()
+                )
+                ()
+    )
+    ()
+;;
+
+(* https://www.chartjs.org/samples/latest/charts/line/basic.html *)
+let param2 =
+  param_t
+    ~type_:(chart_typeToJs `Line)
+    ~data:(data_t
+             ~labels:[|"January"; "February"; "March"; "April"; "May"; "June"; "July"|]
+             ~datasets:([|dataset_t
+                            ~label:"My First dataset"
+                            (* list or string *)
+                            ~backgroundColor:[|"Red";|]
+                            ~borderColor:[|"Red";|]
+                            ~data:[|93.89; 65.79; 22.57; 33.78; 9.61; 31.21; 2.3|]
+                            ~fill:false
+                            ();
+                          dataset_t
+                            ~label:"My Second dataset"
+                            ~fill:false
+                            ~backgroundColor:[|"Blue";|]
+                            ~borderColor:[|"Blue";|]
+                            ~data:[|27.99; 15.09; 91.47; 5.92; 15.75; 92.90; 3.4|]
+                            ()
+                        |]))
+    ~options:(opt_t
+                ~responsive:true
+                ()
+    )
+    ()
+;;
+                             
 let main () =
   let context = match Dom.Document.getElementById "canvas" Dom.document with
     | None -> raise (Not_found_element "canvas is not found")
     | Some canvas ->
        Js.log canvas;
        CanvasElement.getContext2d canvas in
-  let param =
-    param_t
-      ~type_:"bar"
-      ~data:(data_t
-               ~labels:[|"Red"; "Blue"; "Yellow"; "Green"; "Purple"; "Orange"|]
-               ~datasets:([|dataset_t
-                              ~label:"# of Votes"
-                              ~data:[|12.; 19.; 3.; 5.; 2.; 3.|]
-                              ~backgroundColor:[|
-                                "rgba(255, 99, 132, 0.2)";
-                                "rgba(54, 162, 235, 0.2)";
-                                "rgba(255, 206, 86, 0.2)";
-                                "rgba(75, 192, 192, 0.2)";
-                                "rgba(153, 102, 255, 0.2)";
-                                "rgba(255, 159, 64, 0.2)"
-                              |]
-                              ~borderColor:[|
-                                "rgba(255, 99, 132, 1)";
-                                "rgba(54, 162, 235, 1)";
-                                "rgba(255, 206, 86, 1)";
-                                "rgba(75, 192, 192, 1)";
-                                "rgba(153, 102, 255, 1)";
-                                "rgba(255, 159, 64, 1)"
-                              |]
-                              ~borderWidth:1.
-                              ();
-                          |])
-      )
-      ~options:(opt_t
-                  ~scales:(scales_opt_t
-                             ~yAxes: [|
-                               axis_opt_t
-                                 ~ticks:(ticks_opt_t ~beginAtZero:true)
-                             |]
-                  )
-      )
-      () in
+  let param = param2 in
   (* let param = {
    *     type_ = "bar";
    *     data = {
